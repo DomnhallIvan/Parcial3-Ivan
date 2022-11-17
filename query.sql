d054899fd04406b0825b96a9d8c7f91e07b07956
VicPina <47024323+VicPina@users.noreply.github.com>
2022-11-17 10:48:31 -0600

    Yeah mini avance jaja

---
+++ query.sql
@@ -0,0 +1,16 @@
+INSERT INTO Juego (Nombre_del_Juego,Precio,Genero)
+
+
+INSERT INTO Launcher (Nombre_del_Launcher) VALUES
+("Steam")
+("Epic Games")
+("Riot Games")
+("Origin")
+("Nintendo")
+("PlayStore")
+
+INSERT INTO Plataforma(Nombre_de_la_plataforma) VALUES
+("PC")
+("Playstation")
+("Nintendo Switch")
+("Móvil")
